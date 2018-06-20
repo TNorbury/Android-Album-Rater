5aaa837d9a3eb659aff6c652bc4c77253167a902
Tyler Norbury <tylernorbury96@gmail.com>
2018-06-20 15:42:41 -0700

    Filled out the database

--- app/src/main/java/com/tylernorbury/albumrater/database/AlbumDatabase.java
+++ app/src/main/java/com/tylernorbury/albumrater/database/AlbumDatabase.java
@@ -6,14 +6,45 @@
 package com.tylernorbury.albumrater.database;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
+import com.tylernorbury.albumrater.database.dao.AlbumDao;
 import com.tylernorbury.albumrater.database.entity.Album;
 
 /**
- * A database
+ * A database that contains all the album reviews
  */
 @Database(entities = {Album.class}, version = 1)
 public abstract class AlbumDatabase extends RoomDatabase {
 
+    // Gets a singleton instance of the Album DAO
+    public abstract AlbumDao albumDao();
+
+    // The database will be singleton
+    private static AlbumDatabase INSTANCE;
+
+    public static AlbumDatabase getDatabase(final Context context) {
+
+        // If the database hasn't been instantiated, then create it
+        if (INSTANCE == null) {
+
+            // Synchronize the creation to prevent race conditions
+            synchronized (AlbumDatabase.class) {
+
+                // Double check to make sure that the database wasn't created
+                // whilst waiting
+                if (INSTANCE == null) {
+
+                    // Create the database
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AlbumDatabase.class, "albumDatabase")
+                        .build();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
 }
