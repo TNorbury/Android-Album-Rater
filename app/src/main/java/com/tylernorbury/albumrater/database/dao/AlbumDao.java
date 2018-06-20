276082d45ffbd2e89fd83b13e3c0ab86f711759c
Tyler Norbury <tylernorbury96@gmail.com>
2018-06-20 15:12:59 -0700

    Created a DAO for albums

--- app/src/main/java/com/tylernorbury/albumrater/database/dao/AlbumDao.java
+++ app/src/main/java/com/tylernorbury/albumrater/database/dao/AlbumDao.java
@@ -1,9 +1,14 @@
 package com.tylernorbury.albumrater.database.dao;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
 
 import com.tylernorbury.albumrater.database.entity.Album;
+
+import java.util.List;
 
 /**
  * A DAO for accessing the album table
@@ -16,6 +21,25 @@ public interface AlbumDao {
      *
      * @param album The album that has been reviewed
      */
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Album album);
+
+    /**
+     * Gets all the albums that have been reviewed, ordered by the name of the
+     * album
+     * @return All the albums that've been reviewed.
+     */
+    @Query("SELECT title, artist, rating, review, datetime(reviewDate) FROM album " +
+            "ORDER BY title DESC")
+    LiveData<List<Album>> getAllAlbums();
+
+    /**
+     * @return a handful of the most recently reviewed albums
+     */
+    @Query("SELECT title, artist, rating, review, datetime(reviewDate) FROM album " +
+            "ORDER BY datetime(reviewDate) " +
+            "LIMIT 5")
+    LiveData<List<Album>> getRecentAlbums();
+
+    // TODO add some queries for searching for albums by title/artist name
 }
