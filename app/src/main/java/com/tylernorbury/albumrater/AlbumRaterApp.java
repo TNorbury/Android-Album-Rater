869e796377ea55570bf3b21e2079b78791e24fa9
Tyler Norbury <tylernorbury96@gmail.com>
2018-06-22 09:24:38 -0700

    Created a class that handles the app context

---
+++ app/src/main/java/com/tylernorbury/albumrater/AlbumRaterApp.java
@@ -0,0 +1,26 @@
+package com.tylernorbury.albumrater;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Class which serves as an interface to the application context
+ */
+public class AlbumRaterApp extends Application {
+
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+    }
+
+    /**
+     * @return the application's context
+     */
+    public static Context getContext() {
+        return AlbumRaterApp.mContext;
+    }
+
+}
