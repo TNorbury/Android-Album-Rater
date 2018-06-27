d5184786eb0342293dddb5ebca682f16f84669b7
Tyler Norbury <tylernorbury96@gmail.com>
2018-06-27 16:19:51 -0700

    Moved the declaration of the button click listener

--- app/src/main/java/com/tylernorbury/albumrater/fragment/AddAlbumFragment.java
+++ app/src/main/java/com/tylernorbury/albumrater/fragment/AddAlbumFragment.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +24,74 @@ import com.tylernorbury.albumrater.viewModel.AlbumViewModel;
  * create an instance of this fragment.
  */
 public class AddAlbumFragment extends Fragment {
+
+    // When the button is clicked we'll retrieve all the data from the various
+    // forms, create an album object from that, and insert it into the database.
+    Button.OnClickListener mButtonClickListener = new Button.OnClickListener() {
+
+        public void onClick(View v) {
+            int albumRating = -1;
+            boolean validForm = true;
+            View form = getView();
+
+            // Get the name of the album
+            String albumName = ((TextView)form
+                    .findViewById(R.id.add_album_name)).getText().toString();
+
+            // If the album name is blank, tell the user to fill it out
+            if (albumName.isEmpty()) {
+                validForm &= false;
+            }
+
+            // Get the artist of the album
+            String albumArtist = ((TextView)form
+                    .findViewById(R.id.add_album_artist)).getText().toString();
+
+            // If the album artist is blank, tell the user to fill it out
+            if (albumArtist.isEmpty()) {
+                validForm &= false;
+            }
+
+            // Determine the rating of the album
+            int selectedRating = ((RadioGroup)form
+                    .findViewById(R.id.add_album_rating)).getCheckedRadioButtonId();
+
+            // If either the "good" or "bad" rating is selected, set the
+            // corresponding rating
+            if (selectedRating == R.id.add_album_rating_good) {
+                albumRating = Album.GOOD_ALBUM;
+            }
+            else if (selectedRating == R.id.add_album_rating_bad) {
+                albumRating = Album.BAD_ALBUM;
+            }
+
+            // If a -1 was returned, that means that neither option was
+            // selected. In that case tell the user to selected a rating
+            else if (selectedRating == -1) {
+                validForm &= false;
+
+                // Send a toast or something
+            }
+
+
+            // If the form is considered "valid", then we want to insert the
+            // data into the database
+            if (validForm) {
+                Album album = new Album(albumName, albumArtist, albumRating);
+
+                // Get the album view model from the parent activity and then
+                // insert the new album
+                AlbumViewModel model = ViewModelProviders.of(getActivity())
+                        .get(AlbumViewModel.class);
+                model.insert(album);
+
+                // Now "pop" the back stack, returning the user to the last
+                // screen they were on
+                getActivity().getSupportFragmentManager().popBackStack();
+//                    ((BottomNavigationView)getActivity().findViewById(R.id.navigation)).setSelectedItemId(R.id.navigation_home);
+            }
+        }
+    };
 
     public AddAlbumFragment() {
         // Required empty public constructor
@@ -49,58 +118,6 @@ public class AddAlbumFragment extends Fragment {
 
         // Get the button from the view and add an event listener to it
         Button button = view.findViewById(R.id.btn_add_album);
-        button.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                int albumRating = -1;
-                boolean validForm = true;
-                View form = getView();
-
-                // Get the name of the album
-                String albumName = ((TextView)form.findViewById(R.id.add_album_name)).getText().toString();
-
-                // If the album name is blank, tell the user to fill it out
-                if (albumName.isEmpty()) {
-                    validForm &= false;
-                }
-
-                // Get the artist of the album
-                String albumArtist = ((TextView)form.findViewById(R.id.add_album_artist)).getText().toString();
-
-                // If the album artist is blank, tell the user to fill it out
-                if (albumArtist.isEmpty()) {
-                    validForm &= false;
-                }
-
-                // Determine the rating of the album
-                int selectedRating = ((RadioGroup)form.findViewById(R.id.add_album_rating)).getCheckedRadioButtonId();
-
-                // If either the "good" or "bad" rating is selected, set the
-                // corresponding rating
-                if (selectedRating == R.id.add_album_rating_good) {
-                    albumRating = Album.GOOD_ALBUM;
-                }
-                else if (selectedRating == R.id.add_album_rating_bad) {
-                    albumRating = Album.BAD_ALBUM;
-                }
-
-                // If a -1 was returned, that means that neither option was
-                // selected. In that case tell the user to selected a rating
-                else if (selectedRating == -1) {
-                    validForm &= false;
-
-                    // Send a toast or something
-                }
-
-
-                // If the form is considered "valid", then we want to insert the data into the database
-                if (validForm) {
-                    Album album = new Album(albumName, albumArtist, albumRating);
-
-                    // Get the album view model from the parent activity and then insert the new album
-                    AlbumViewModel model = ViewModelProviders.of(getActivity()).get(AlbumViewModel.class);
-                    model.insert(album);
-                }
-            }
-        });
+        button.setOnClickListener(mButtonClickListener);
     }
 }
